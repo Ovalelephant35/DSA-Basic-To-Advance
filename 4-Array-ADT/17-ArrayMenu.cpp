6b63555d88ebe01790b56a0c6a50a2d83760ac49
Anurag Singh <principalquantum30@gmail.com>
2023-10-18 22:22:25 +0530

    Arrays

--- 4-Array-ADT/17-ArrayMenu.cpp
+++ 4-Array-ADT/17-ArrayMenu.cpp
@@ -4,9 +4,11 @@
 #include<stdio.h>
 using namespace std;
 class Array{
+    private:
     int *A;
     int size;
     int len;
+    public:
     Array(){
         size=10;
         len=0;
@@ -21,12 +23,12 @@ class Array{
         delete []A;
     }
     void display(){
-        for(int i=0 ; i<len ; i++){
+        for(int i=0 ; i<size ; i++){
             cout<<A[i]<<" ";
         }
     }
     void insert(int index , int num){
-        if(index>=0 && len<size && index<size){
+        if(index>=0 && len<size && index<=len){
             int i=len-1;
             while(i>=index){
                 A[i+1] = A[i];
@@ -39,24 +41,37 @@ class Array{
     void deletion(int index){
         if(index>=0 && index<len){
             int i=index;
-            while(i<len){
+            while(i<len-1){
                 A[i] =A[i+1];
                 i++;
             }
             len--;
         }
     }
-    
-
-}
-
-
-
-
-
-
-
+    bool issorted(){
+        for(int i=0 ; i<len-1 ; i++){
+            if(A[i]>A[i+1])return false;
+        }
+        return true;
+    }
+};
 int main(){
-   
+   Array arr(10);
+   arr.insert(0,1);
+   arr.insert(1,2);
+   arr.insert(2,3);
+   arr.insert(3,4);
+   arr.insert(4,5);
+   arr.insert(5,6);
+   arr.insert(6,7);
+   arr.insert(7,8);
+   arr.insert(8,9);
+   arr.insert(9,10);
+   arr.insert(10,110);
+   arr.display();
+   cout<<arr.issorted()<<endl;
+   arr.deletion(0);
+   arr.insert(0 ,1);
+   arr.display();
 return 0;
 }
