14029ab07b98d57922d534206ca1a1935dca60a9
Anurag Singh <principalquantum30@gmail.com>
2023-10-26 21:06:38 +0530

    Matrices

---
+++ 6-Matrices/4-Column-Lower-Representation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<stdio.h>
+using namespace std;
+class low{
+    int n;
+    int *A;
+    low(int ne){
+        n=ne;
+        A= new int[n*(n+1)];
+    }
+    void set(int i , int j , int x){
+        if(i<=
+    }
+
+
+}
+
+int main(){
+   
+return 0;
+}
